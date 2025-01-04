871069930c62188cc6436098ab9528e408c1b843
Sergey Vernigora <volt.awp@gmail.com>
2025-01-04 12:26:30 +0200

    update(scripts): added condition for creating dhparams into `run.sh` script.
    update(nginx): updated server name in upstream directive for staging server.

--- backend/deploy_config/nginx/run.sh
+++ backend/deploy_config/nginx/run.sh
@@ -6,7 +6,10 @@ set -e
 export host=\$host
 export request_uri=\$request_uri
 
-openssl dhparam -out /etc/nginx/ssl-dhparams.pem 2048
+if [ ! -f "/etc/nginx/ssl-dhparams.pem" ]; then
+    echo "dhparams.pem doesn't exist - creating it..."
+    openssl dhparam -out /etc/nginx/ssl-dhparams.pem 2048
+fi
 
 envsubst '${DOMAIN} ${SERVER_PORT}' </etc/nginx/modem_proxy.conf.template >/etc/nginx/conf.d/modem_proxy.conf
 
