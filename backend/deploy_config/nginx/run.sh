dd1aa9ea804c14de7b27b21fd025cff136b01ecc
Sergey V. <volt.awp@gmail.com>
2025-01-04 13:08:27 +0200

    update(scripts): updated 'run.sh' script.

--- backend/deploy_config/nginx/run.sh
+++ backend/deploy_config/nginx/run.sh
@@ -2,14 +2,14 @@
 
 set -e
 
+if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
+    echo "dhparams.pem doesn't exist - creating it..."
+    openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
+fi
+
 # Avoid replacing these with envsubst
 export host=\$host
 export request_uri=\$request_uri
-
-if [ ! -f "/etc/nginx/ssl-dhparams.pem" ]; then
-    echo "dhparams.pem doesn't exist - creating it..."
-    openssl dhparam -out /etc/nginx/ssl-dhparams.pem 2048
-fi
 
 envsubst '${DOMAIN} ${SERVER_PORT}' </etc/nginx/modem_proxy.conf.template >/etc/nginx/conf.d/modem_proxy.conf
 
