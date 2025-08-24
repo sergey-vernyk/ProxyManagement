ce39a2173d430e0ecc793f3b50a1558905e220cc
Sergey Vernigora <115031024+sergey-vernyk@users.noreply.github.com>
2025-08-24 21:56:06 +0300

    Fix CLI for running web server
    Added 'BASE_DIR' variable because the server must be run from the base dir. 'static' and 'template' dirs works well if the base dir is correct.
    
    Added loading env vars before the server run. Without it an error occurs even if path to .env file was provided.

--- src/proxy_management/cli/web_server.py
+++ src/proxy_management/cli/web_server.py
@@ -1,20 +1,24 @@
+import os
 import pathlib
+from pathlib import Path
 
 import click
 import uvicorn
+from dotenv import load_dotenv
+
+# build path to the base directory of the package (proxy_management)
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 @click.group(help="CLI provides functionality for running uvicorn server with parameters.")
 def cli_web() -> None:
-    """
-    CLI entrypoint for web application.
-    """
+    """CLI entrypoint for web application."""
 
 
 @click.command(help="Run the FastAPI server with the provided options.")
 @click.option("--host", "-h", type=click.STRING, show_default=True, default="0.0.0.0", help="Server host.")
-@click.option("--port", "-p", type=click.INT, show_default=True, default="8000", help="Server port.")
-@click.option("--workers", type=click.INT, show_default=True, default="2", help="Number of worker processes.")
+@click.option("--port", "-p", type=click.INT, show_default=True, default=8000, help="Server port.")
+@click.option("--workers", type=click.INT, show_default=True, default=2, help="Number of worker processes.")
 @click.option("--env-file", type=click.Path(exists=True), required=True, help="Environment configuration file.")
 def runserver(workers: int, host: str, port: int, env_file: pathlib.Path) -> None:
     """
@@ -22,21 +26,24 @@ def runserver(workers: int, host: str, port: int, env_file: pathlib.Path) -> Non
 
     Args:
         workers (int): Number of worker processes for handling requests.
-        env_file (Optional[pathlib.Path]): Path to the environment configuration file.
+        env_file (pathlib.Path): Path to the environment configuration file.
         host (str): The server host address.
         port (int): The port to bind the server.
 
     This command sets up and runs the FastAPI server with configurable settings such as
     host, port, and workers. It also optionally reads an environment configuration file if specified.
     """
+    load_dotenv(env_file, override=True)
+
     config = {
-        "app": "main:app",
+        "app": "proxy_management.main:app",
         "host": host,
         "port": port,
         "workers": workers,
-        "env_file": env_file,
     }
 
+    # explicitly change directory to base which is 'proxy_management'
+    os.chdir(BASE_DIR)
     uvicorn.run(**{key: value for key, value in config.items() if value is not None})
 
 
